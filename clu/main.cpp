8e780da6071e14e397c11c6ee5b608337353d99e
Vlad Shevchenko <vlad.shevchenko.ua@gmail.com>
2016-05-26 10:25:30 +0300

    Implemented clu

--- clu/main.cpp
+++ clu/main.cpp
@@ -2,18 +2,19 @@
 #include <QTextStream>
 #include <iostream>
 
-#include <bitset.h>
 #include <expression_evaluator.h>
 
 int main(int argc, char *argv[])
 {
     //QCoreApplication a(argc, argv);
     QTextStream cout(stdout);
-    QString in(argv[1]);
 
-    BitSet bs1(in), bs2("1010101");
+    QString expression;
+    for (int i = 1; i < argc; ++i) {
+        expression += argv[i];
+    }
 
-    cout << (bs1 & bs2).toString();
+    cout << (new ExpressionEvaluator(expression))->evaluate().toString();
 
     //return a.exec();
 }
